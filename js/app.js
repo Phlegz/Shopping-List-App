f6f52924bce89ce88f058fdffd428fb6135b37fd
Farnaz Rashidi <farnaz.rashidi@gmail.com>
2015-10-13 00:15:12 -0700

    commit

--- js/app.js
+++ js/app.js
@@ -5,7 +5,15 @@ $(document).ready(function(){
 		//$('<li>').text(item).appendTo('.items');
 		$('.items').append('<li><div class="unchecked"></div>' + "<span>"+$('#input').val()+ "</span>" + '<div class="delete"></div></li>');
 		$('#input').val("");
+
+		
 	});
+
+
+	$(".unchecked").click(function(){
+
+	});
+
 
 	$("ul:first").click(function(){
 		//$(this).toggleClass("checked-box")
