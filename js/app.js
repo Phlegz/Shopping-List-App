0c6912b077607996e8a9f1fe9e5cc12988a90199
Farnaz Rashidi <farnaz.rashidi@gmail.com>
2015-10-15 00:46:37 -0700

    new app.js

--- js/app.js
+++ js/app.js
@@ -3,12 +3,19 @@ $(document).ready(function(){
 	$("#button").click(function(){
 		var item=$('#input').val();
 		//$('<li>').text(item).appendTo('.items');
-		$('.items').append('<li><div class="unchecked"></div>' + "<span>"+$('#input').val()+ "</span>" + '<div class="delete"></div></li>');
+		var $listItem = $('<li><div class="unchecked"></div>' + "<span>"+$('#input').val()+ "</span>" + '<div class="delete"></div></li>');
+		$listItem.click(function(){
+			//$(this).toggleClass("checked-box")
+	        // $(".unchecked").toggleClass("checked");
+	        $(this).find('div:first-child').toggleClass("checked");
+		});
+
+		$('.items').append($listItem);
 		$('#input').val("");
 	});
 
-	$("ul:first").click(function(){
-		//$(this).toggleClass("checked-box")
-        $(".unchecked").toggleClass("checked");
-	});
+	// $("ul:first").click(function(){
+	// 	//$(this).toggleClass("checked-box")
+ //        $(".unchecked").toggleClass("checked");
+	// });
 });
